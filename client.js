c4fb018994cbc8f0ba4246e8081cc5ee3e5991f0
Ruben Oostinga <roostinga@xebia.com>
2015-12-11 10:39:45 +0100

    Add images using vanilla JS

--- client.js
+++ client.js
@@ -13,3 +13,22 @@ model
   .then(function(surname) {
     console.log(surname);
   });
+
+renderTiles([{
+  title: 'falcor',
+  src: 'https://zemanifesto.files.wordpress.com/2014/07/bravecor1.jpg?w=590&h=442'
+}]);
+
+function renderTiles(tiles) {
+  const contentDiv = document.querySelector('#content');
+  tiles
+    .map(tile => tileToDom(tile))
+    .forEach(domTile => contentDiv.appendChild(domTile))
+}
+
+function tileToDom(tile) {
+  const img = document.createElement('img');
+  img.src = tile.src;
+  img.alt = tile.title;
+  return img;
+}
