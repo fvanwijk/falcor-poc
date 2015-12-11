49b03053f5b3292fc460c9c50f599bfc68f6fea5
Ruben Oostinga <roostinga@xebia.com>
2015-12-11 12:27:59 +0100

    Filter by range serverside

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+'use strict';
 // index.js
 var falcorExpress = require('falcor-express');
 var Router = require('falcor-router');
@@ -6,18 +7,27 @@ var fetch = require('node-fetch');
 var express = require('express');
 var app = express();
 
+function filter(pokemons, range) {
+  const result = [];
+  for(var i = range.from; i <= range.to; i++) {
+    result.push(pokemons[i]);
+  }
+  return result;
+}
+
 app.use('/model.json', falcorExpress.dataSourceRoute((req, res) => {
   // create a Virtual JSON resource with single key ("greeting")
   return new Router([
     {
       // match a request for the key "greeting"
-      route: 'pokemon',
+      route: 'pokemon[{ranges:indexRanges}]',
       // respond with a PathValue with the value of "Hello World."
-      get() {
+      get(pathSet) {
+        const indexRange = pathSet.indexRanges[0];
         return fetch('http://pokeapi.co/api/v1/pokedex/1/')
           .then(response => response.json())
           .then(response => ({
-            path: ['pokemon'], value: response.pokemon
+            path: ['pokemon'], value: filter(response.pokemon, indexRange)
           }));
       }
     },{
