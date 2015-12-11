936e0fa84f4cedb25fba9c1e7d2f255ad6f66f17
Ruben Oostinga <roostinga@xebia.com>
2015-12-11 11:32:26 +0100

    Use node fetch

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 // index.js
 var falcorExpress = require('falcor-express');
 var Router = require('falcor-router');
-var request = require('request-promise');
+var fetch = require('node-fetch');
 
 var express = require('express');
 var app = express();
 
-app.use('/model.json', falcorExpress.dataSourceRoute(function (req, res) {
+app.use('/model.json', falcorExpress.dataSourceRoute((req, res) => {
   // create a Virtual JSON resource with single key ("greeting")
   return new Router([
     {
       // match a request for the key "greeting"
       route: 'greeting',
       // respond with a PathValue with the value of "Hello World."
-      get: function () {
-        return request('http://pokeapi.co/api/v1/pokemon/1').then( (response) => {
-          return { path: ['greeting'], value: response };
-        });
+      get() {
+        return fetch('http://pokeapi.co/api/v1/pokemon/1')
+          .then(response => response.json())
+          .then(value => ({
+            path: ['greeting'], value
+          }));
       }
     }
   ]);
